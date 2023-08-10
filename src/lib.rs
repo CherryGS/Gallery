23929f5d0abc8b0e68fd90577e16b1364324f843
CherryGS <tickten@outlook.com>
2023-08-10 16:45:37 +0800

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![warn(clippy::all, rust_2018_idioms)]
-
 mod app;
+mod utils;
+
 pub use app::TemplateApp;
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,20 @@
-#![warn(clippy::all, rust_2018_idioms)]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+// #![warn(clippy::all, rust_2018_idioms)]
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    // 注册到 `stderr`
+    env_logger::init();
 
-    let native_options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "eframe template",
+    // 原生 gui 设置
+    let native_options = eframe::NativeOptions {
+        ..Default::default()
+    };
+
+    // 运行 egui
+    return eframe::run_native(
+        "gallery",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
-    )
-}
-
-// When compiling to web using trunk:
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    // Redirect `log` message to `console.log` and friends:
-    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
-
-    let web_options = eframe::WebOptions::default();
-
-    wasm_bindgen_futures::spawn_local(async {
-        eframe::WebRunner::new()
-            .start(
-                "the_canvas_id", // hardcode it
-                web_options,
-                Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
-            )
-            .await
-            .expect("failed to start eframe");
-    });
+        Box::new(|cc| Box::new(gallery::TemplateApp::new(cc))),
+    );
 }
